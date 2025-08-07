1f3d20d40e90def3492dcf0eb7ad54ba151ae3dc
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-08-07 20:58:37 +0800

    Added images category.

--- lib/data/api/pixabay_api.dart
+++ lib/data/api/pixabay_api.dart
@@ -5,7 +5,7 @@ import 'http_request.dart';
 
 Future<Response?> getImage(String query) async {
   var url = "https://pixabay.com/api/?key=$pixabayApiKey&q=$query=photo"
-      "&image_type=photo&pretty=true";
+      "&image_type=photo&category=food";
   var response = await getHttpRequest(url, {});
   if (response != null) {
     return response;
